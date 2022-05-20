f5ab6c9ffe9d47d4703cde5dbec6ef9c36eebdae
Spencer-16 <spencerlrc16@gmail.com>
2022-05-20 17:58:01 -0400

    Update the payload_checker to rebuilt tensors transfered

--- plato/processors/payload_checker.py
+++ plato/processors/payload_checker.py
@@ -21,12 +21,19 @@ class Processor(model.Processor):
         self.context = homo_enc.get_ckks_context()
 
     def process(self, data: Any) -> Any:
+        """
+        Only conv1.weight tensor is operated for now.
+        """
         output = data
         print(type(data))
         rebuilt_vector = ts.lazy_ckks_tensor_from(output['conv1.weight'])
         rebuilt_vector.link_context(self.context)
         rebuilt_tensor = torch.tensor(rebuilt_vector.decrypt().tolist())
-        print(rebuilt_tensor)
+        #print(rebuilt_tensor)
+        rebuilt_tensor=rebuilt_tensor.reshape(output['conv1.weight.shape'])
+        #print(rebuilt_tensor)
+        output['conv1.weight'] = rebuilt_tensor
+        output.pop('conv1.weight.shape')
         #print(data[str(list(data)[0])])
         return output
 
