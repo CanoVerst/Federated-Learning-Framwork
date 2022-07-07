c302ce8ef52e8ebd00d5787ec818cc86ee9d03f2
Chenghao <ch.hu@mail.utoronto.ca>
2022-07-06 22:37:37 -0400

    Added a client and server follow MaskCrypt design

--- plato/clients/simple_he.py
+++ plato/clients/simple_he.py
@@ -4,13 +4,19 @@ A basic federated learning client with homomorphic encryption support
 
 import logging
 import time
+import sys
+import torch
+import numpy as np
+import pickle
 from dataclasses import dataclass
+from typing import overload
 
 from plato.algorithms import registry as algorithms_registry
 from plato.clients import simple
 from plato.config import Config
 from plato.datasources import registry as datasources_registry
 from plato.processors import registry as processor_registry
+from plato.processors.model_encrypt import Processor as encrpt_processor
 from plato.samplers import registry as samplers_registry
 from plato.trainers import registry as trainers_registry
 from plato.utils import fonts
@@ -24,4 +30,121 @@ class Client(simple.Client):
                  algorithm=None,
                  trainer=None):
         super().__init__(model = model, datasource = datasource, algorithm = algorithm, trainer = trainer)
+
+        self.model_buffer = []
+    
+    def get_exposed_weights(self):
+        # Read exposed weights from file
+        # Return original model weights as a place holder
+        return self.algorithm.extract_weights()
+
+    def compute_mask(self, exposed_weights, latest_weights, gradients):
+        mask_ratio = 0.05
+        exposed_flat = torch.cat([torch.flatten(exposed_weights[name]) for _, name 
+                                    in enumerate(exposed_weights)]) 
+        latest_flat = torch.cat([torch.flatten(latest_weights[name]) for _, name 
+                                    in enumerate(latest_weights)]) 
+        grad_flat = torch.cat([torch.flatten(gradients[name]) for _, name 
+                                    in enumerate(gradients)]) 
         
+        delta = exposed_flat - latest_flat + torch.randn(len(latest_flat))
+        product = delta * grad_flat
+        
+        sorted_product, indices = torch.sort(product, descending = True)
+        positive_number = torch.sum(product > 0)
+        mask_len = int(mask_ratio * len(indices))
+    
+        return indices[:min(positive_number, mask_len)].clone().detach()
+
+
+    async def payload_to_arrive(self, response):
+        assert self.comm_simulation
+        self.current_round = response['current_round']
+        self.client_id = response['id']
+
+        payload_filename = response['payload_filename']
+        with open(payload_filename, 'rb') as payload_file:
+            self.server_payload = pickle.load(payload_file)
+
+        payload_size = sys.getsizeof(pickle.dumps(self.server_payload))
+
+        logging.info(
+            "[%s] Received %.2f MB of payload data from the server (simulated).",
+            self, payload_size / 1024**2)
+
+        if self.current_round % 2 != 0:
+        
+            #print(response)
+            # Update (virtual) client id for client, trainer and algorithm
+            
+
+            self.process_server_response(response)
+
+            self.configure()
+
+            logging.info("[Client #%d] Selected by the server.", self.client_id)
+
+            self.server_payload = self.inbound_processor.process(
+            self.server_payload)
+
+            if not hasattr(Config().data,
+                        'reload_data') or Config().data.reload_data:
+                self.load_data()
+            await self.start_training()
+
+            mask_proposal = self.compute_mask(self.get_exposed_weights(), 
+                                              self.algorithm.extract_weights(),
+                                              self.trainer.gradient)
+            # Send mask_proposal to server
+            await self.send(mask_proposal, process = False)
+
+        else:
+            mask = self.server_payload
+            client_id, report, payload = self.model_buffer.pop()
+            await self.sio.emit('client_report', {
+            'id': response['id'],
+            'report': pickle.dumps(report)
+            })
+            for processor in self.outbound_processor.processors:
+                if isinstance(processor, encrpt_processor):
+                    processor.encrypt_mask = mask
+            await self.send(payload, process = True)
+            
+    
+    
+
+    async def start_training(self):
+        """ Overwrite training function. """
+        self.load_payload(self.server_payload)
+        self.server_payload = None
+
+        report, payload = await self.train()
+
+        # Sending the client report as metadata to the server (payload to follow)
+        await self.sio.emit('client_report', {
+            'id': self.client_id,
+            'report': pickle.dumps(report)
+        })
+
+        self.model_buffer.append((self.client_id, report, payload))
+
+
+    async def send(self, payload, process = True):
+        """Sending the client payload to the server using simulation, S3 or socket.io."""
+        # First apply outbound processors, if any
+        if process:
+            payload = self.outbound_processor.process(payload)
+
+        if self.comm_simulation:
+            # If we are using the filesystem to simulate communication over a network
+            model_name = Config().trainer.model_name if hasattr(
+                Config().trainer, 'model_name') else 'custom'
+            checkpoint_path = Config().params['checkpoint_path']
+            payload_filename = f"{checkpoint_path}/{model_name}_client_{self.client_id}.pth"
+            with open(payload_filename, 'wb') as payload_file:
+                pickle.dump(payload, payload_file)
+
+            logging.info(
+                "[%s] Sent %.2f MB of payload data to the server (simulated).",
+                self,
+                sys.getsizeof(pickle.dumps(payload)) / 1024**2)
--- plato/servers/fedavg_he.py
+++ plato/servers/fedavg_he.py
@@ -20,6 +20,9 @@ class Server(fedavg.Server):
         self.para_nums = {}
         self.ckks_context = homo_enc.get_ckks_context()
 
+        self.final_mask = None
+
+
     def init_trainer(self):
         """Setting up the global model to be trained via federated learning."""
         super().init_trainer()
@@ -34,42 +37,67 @@ class Server(fedavg.Server):
                                                         encrypt_ratio = 0)
             
 
+    def mask_consensus(self, updates):
+        proposals = [mask for (__, __, mask, __) in updates]
+        mask_size = len(proposals[0])
+        interleaved_indices = torch.zeros((sum([len(x) for x in proposals])))
+        for i in range(len(proposals)):
+            interleaved_indices[i::len(proposals)] = proposals[i]
+        
+        _, indices = interleaved_indices.unique(sorted=False, return_inverse = True)
+        
+        self.final_mask = interleaved_indices[indices.unique()[:mask_size]].clone().detach()
+        self.final_mask = self.final_mask.int()
+
     async def aggregate_weights(self, updates):
         """Aggregate the reported weight updates from the selected clients."""
-        self.encrypted_model = await self.federated_averaging_he(updates)
+        if self.current_round % 2 != 0:
+            self.mask_consensus(updates)
+        else:
+            self.encrypted_model = await self.federated_averaging_he(updates)
 
-        # Decrypt model weights for test accuracy
-        decrypted_weights = homo_enc.decrypt_weights(self.encrypted_model, 
-                                                     self.weight_shapes, self.para_nums)
-        self.algorithm.load_weights(decrypted_weights)
+            # Decrypt model weights for test accuracy
+            decrypted_weights = homo_enc.decrypt_weights(self.encrypted_model, 
+                                                         self.weight_shapes, self.para_nums)
+            self.algorithm.load_weights(decrypted_weights)
+
+            self.encrypted_model["encrypted_weights"] = self.encrypted_model["encrypted_weights"].serialize()
 
     async def federated_averaging_he(self, updates):
         """Aggregate weight updates from the clients using federated averaging."""
-        weights_received = [payload for (__, __, payload, __) in updates]
+        weights_received = [homo_enc.deserialize_weights(payload, self.ckks_context)  
+                               for (__, __, payload, __) in updates]
+
+        unencrypted_weights = [x["unencrypted_weights"] for x in weights_received]
+        encrypted_weights = [x["encrypted_weights"] for x in weights_received]
 
         # Extract the total number of samples
         self.total_samples = sum(
             [report.num_samples for (__, report, __, __) in updates])
 
         # Perform weighted averaging
-        avg_update = {
-            name: self.trainer.zeros(weights.size())
-            for name, weights in weights_received[0].items()
-        }
+        unencrypted_avg_update = self.trainer.zeros(unencrypted_weights[0].size())
+        encrypted_avg_update = self.trainer.zeros(encrypted_weights[0].size())
 
-        for i, update in enumerate(weights_received):
+        for i, (unenc_w, enc_w) in enumerate(zip(unencrypted_weights, encrypted_weights)):
             __, report, __, __ = updates[i]
             num_samples = report.num_samples
 
-            for name, delta in update.items():
-                # Use weighted average by the number of samples
-                avg_update[name] += delta * (num_samples / self.total_samples)
+            unencrypted_avg_update += unenc_w * (num_samples / self.total_samples)
+            encrypted_avg_update += enc_w * (num_samples / self.total_samples)
 
             # Yield to other tasks in the server
             await asyncio.sleep(0)
 
-        return avg_update
+        avg_result = OrderedDict()
+        avg_result["unencrypted_weights"] = unencrypted_avg_update
+        avg_result["encrypted_weights"] = encrypted_avg_update
+        avg_result["encrypt_indices"] = self.final_mask
+        return avg_result
 
     def customize_server_payload(self, payload):
-        """ Customize the server payload before sending to the client. """        
-        return self.encrypted_model
+        """ Customize the server payload before sending to the client. """
+        if self.current_round % 2 != 0:
+            return self.encrypted_model
+        else:
+            return self.final_mask
